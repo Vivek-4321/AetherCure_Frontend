69d33ecf31e04febf7fbd572683d6269b7229ed4
Vivek-4321 <vivekvenugopal514@gmail.com>
2025-03-28 04:59:07 +0530

    Api changed

--- src/components/DataCollection.vue
+++ src/components/DataCollection.vue
@@ -127,7 +127,7 @@ onMounted(async () => {
     
     // In a real app, these would be accessed from the environment
     mainApiBaseUrl.value ='https://high-goose-81-mh33xaffavj3.deno.dev';
-    blockchainApiBaseUrl.value = 'http://174.129.61.3:3000';
+    blockchainApiBaseUrl.value = 'https://aethercure.site';
     
     // Fetch user data
     await fetchUserData()
